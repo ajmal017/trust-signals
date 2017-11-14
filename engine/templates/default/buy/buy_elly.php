6e80513ce512345903a483adf35b5860e6addc6f
janickiy <janickiy@mail.ru>
2017-11-14 17:39:00 +0300

    bug fix

--- engine/templates/default/buy/buy_elly.php
+++ engine/templates/default/buy/buy_elly.php
@@ -1,251 +1,209 @@
-﻿
-<div class="col-md-12">
-	<div class="row">
-		<div class="col-md-12"></div>
-	</div>
-	<div id="open-pay-systems">
-		<div class="pay-system-item-info">
-			<h4><span class="glyphicon glyphicon-credit-card"></span> Сумма к оплате: <strong><span id="systems-summ"></span></strong>$</h4>
-			<h4><span class="glyphicon glyphicon-briefcase"></span> Тип пакета: <strong>РОБОТ</strong></h4>
-		</div>
-		<ul class="text-center nav nav-pills" role="tablist">
-			%systems%
-		</ul>
-		<div class="tab-content" style="z-index: 2;">
-			%list_systems%
-			<div class="form-group col-md-12 col-sm-12 col-lg-12">
-				<button class="btn btn-warning disabled btn-lg" id="generation-link">Оплатить</button>
-			</div>
-		</div>
-	</div>
+﻿<div class="col-md-12">
+    <div class="row">
+        <div class="col-md-12"></div>
+    </div>
+    <div id="open-pay-systems">
+        <div class="pay-system-item-info">
+            <h4><span class="glyphicon glyphicon-credit-card"></span> Сумма к оплате: <strong><span id="systems-summ"></span></strong>$</h4>
+            <h4><span class="glyphicon glyphicon-briefcase"></span> Тип пакета: <strong>РОБОТ</strong></h4>
+        </div>
+        <ul class="text-center nav nav-pills" role="tablist">
+            %systems%
+        </ul>
+        <div class="tab-content" style="z-index: 2;">
+            %list_systems%
+            <div class="form-group col-md-12 col-sm-12 col-lg-12">
+                <button class="btn btn-warning disabled btn-lg" id="generation-link">Оплатить</button>
+            </div>
+        </div>
+    </div>
 </div>
 <style media="screen">
-	.pay-system-item {
-		float: none !important;
-	}
+    .pay-system-item {
+        float: none !important;
+    }
 </style>
 <!--  PayPal  -->
-
 <div class="modal fade" id="paypal-system" tabindex="-1" role="dialog" aria-labelledby="paypal-system-label" style="z-index: 500000;">
-	<div class="modal-dialog" role="document">
-		<div class="modal-content">
-			<div class="modal-header">
-				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
-				<h4 class="modal-title" id="myModalLabel">Оплата через PayPal</h4>
-			</div>
-			<div class="modal-body" id="chart-box-modal">
-				Оплата через PayPal в разработке
-			</div>
-		</div>
-	</div>
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel">Оплата через PayPal</h4>
+            </div>
+            <div class="modal-body" id="chart-box-modal">
+                Оплата через PayPal в разработке
+            </div>
+        </div>
+    </div>
 </div>
 <!--  YandexMoney  -->
 <div style="overflow-y: auto;" class="modal fade" id="yandex-system" tabindex="-1" role="dialog" aria-labelledby="yandex-system-label" style="z-index: 500000;">
-	<div class="modal-dialog" role="document">
-		<div class="modal-content">
-			<div class="modal-header">
-				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
-				<h4 class="modal-title" id="myModalLabel">Оплата через YandexMoney</h4>
-			</div>
-			<div class="modal-body" id="chart-box-modal" style="height: 100%;">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel">Оплата через YandexMoney</h4>
+            </div>
+            <div class="modal-body" id="chart-box-modal" style="height: 100%;">
 
-			<div class="row">
- 
-
-<style>
-.payya{padding: 5px;border: solid 4px white;
-
-    background-color: #fbfbfb;}
-	.payya:hover {border: solid 5px #009688;
-	transition:all 0.3s ease;}
-</style>
-
-<div class="hidden-xs col-md-12" style="margin-top: 10px;">
-<center style="">
-<!--  VIP TARIF  -->
-<div class="hidden-xs col-md-12" style="margin-bottom: 10px; margin-top: -10px; font-size: 16px;">Тарифы на <i class="fa fa-diamond" aria-hidden="true"></i> VIP аккаунты</div>
-<a href="http://paykasa24.com/payment/go/?transaction=vip7&item=VIP%20%D0%BA%D0%B0%D0%B1%D0%B8%D0%BD%D0%B5%D1%82%20trust-signals.com%20%D0%BD%D0%B0%207%20%D0%B4%D0%BD%D0%B5%D0%B9.&amount=1999">
-<div class="col-sm-4 payya " style=""><i class="fa fa-diamond" aria-hidden="true"></i> <span style="font-size:16px">Vip</span> аккаунт на 7 дней <br><center> 
-   <i class="fa fa-shopping-cart" aria-hidden="true" style="color: grey;
-    font-size: 16px;"></i> <span style="font-size: 22px;
-    color: #607D8B;">1999 RUB</span> <span style="color:green;">(35$)</span><br> <b style="color:#8BC34A;">ОПЛАТИТЬ</b></center>
-	</div></a>
-	
-	<a href="http://paykasa24.com/payment/go/?transaction=vip30&item=VIP%20%D0%BA%D0%B0%D0%B1%D0%B8%D0%BD%D0%B5%D1%82%20trust-signals.com%20%D0%BD%D0%B0%2030%20%D0%B4%D0%BD%D0%B5%D0%B9.&amount=3420">
-<div class="col-sm-4 payya " style=""><i class="fa fa-diamond" aria-hidden="true"></i> <span style="font-size:16px">Vip</span> аккаунт на 30 дней <br><center> 
-   <i class="fa fa-shopping-cart" aria-hidden="true" style="color: grey;
-    font-size: 16px;"></i> <span style="font-size: 22px;
-    color: #607D8B;">3420 RUB</span> <span style="color:green;">(60$)</span><br> <b style="color:#8BC34A;">ОПЛАТИТЬ</b></center>
-	</div></a>
-	<a href="http://paykasa24.com/payment/go/?transaction=vip1&item=VIP%20%D0%BA%D0%B0%D0%B1%D0%B8%D0%BD%D0%B5%D1%82%20trust-signals.com%20%D0%BD%D0%B0%201%20%D0%93%D0%BE%D0%B4.&amount=5650">
-<div class="col-sm-4 payya " style=""><i class="fa fa-diamond" aria-hidden="true"></i> <span style="font-size:16px">Vip</span> аккаунт на 1 ГОД <br><center> 
-   <i class="fa fa-shopping-cart" aria-hidden="true" style="color: grey;
-    font-size: 16px;"></i> <span style="font-size: 22px;
-    color: #607D8B;">5650 RUB</span> <span style="color:green;">(99$)</span><br> <b style="color:#8BC34A;">ОПЛАТИТЬ</b></center>
-	</div></a>
-<!--  BASE TARIF  -->
-
-
-<div class="hidden-xs col-md-12" style=" margin-top: 15px; font-size: 16px;">Тарифы на <i class="fa fa-suitcase" aria-hidden="true"></i> Базовые аккаунты</div>
-<div class="hidden-xs col-md-12" style="margin-top: 10px;">
+                <div class="row">
+                    <div class="col-md-6" style="    background-color: #E6463A;
+	color: white;
+	padding: 10px;"><b>Тарифы VIP раздела.</b><br>
+                        На 1 неделю -- 1500 рублей <br>
+                        На 1 месяц + ROBOT -- 2699 рублей <br>
+                        На 1 Год + ROBOT -- 6999 рублей </div>
 
 
 
 
-<a href="http://paykasa24.com/payment/go/?transaction=24&item=%D0%91%D0%B0%D0%B7%D0%BE%D0%B2%D1%8B%D0%B9%20%D0%BA%D0%B0%D0%B1%D0%B8%D0%BD%D0%B5%D1%82%20trust-signals.com%20%D0%BD%D0%B0%2024%20%D1%87%D0%B0%D1%81%D0%B0.&amount=405">
-<div class="col-sm-4 payya " style=""><i class="fa fa-suitcase" aria-hidden="true"></i> <span style="font-size:16px">Базовый</span> аккаунт на 1 день <br><center> 
-   <i class="fa fa-shopping-cart" aria-hidden="true" style="color: grey;
-    font-size: 16px;"></i> <span style="font-size: 22px;
-    color: #607D8B;">405 RUB</span> <span style="color:green;">(7$)</span><br> <b style="color:color:#FF9800;">ОПЛАТИТЬ</b></center>
-	</div></a>
+                    <div class="col-md-6" style="background-color: #009688;
+	color: white;
+	padding: 10px;"><b>Тарифы базавого раздела.</b><br>
+                        3 дня -- 299 рублей <br>
+                        7 дней -- 499 рублей <br>
+                        30 дней-- 899 рублей </div>
 
-<a href="http://paykasa24.com/payment/go/?transaction=2&item=%D0%91%D0%B0%D0%B7%D0%BE%D0%B2%D1%8B%D0%B9%20%D0%BA%D0%B0%D0%B1%D0%B8%D0%BD%D0%B5%D1%82%20trust-signals.com%20%D0%BD%D0%B0%202%20%D0%B4%D0%BD%D1%8F.&amount=570">
-<div class="col-sm-4 payya " style=""><i class="fa fa-suitcase" aria-hidden="true"></i> <span style="font-size:16px">Базовый</span> аккаунт на 2 дня <br><center> 
-   <i class="fa fa-shopping-cart" aria-hidden="true" style="color: grey;
-    font-size: 16px;"></i> <span style="font-size: 22px;
-    color: #607D8B;">570 RUB</span> <span style="color:green;">(10$)</span><br> <b style="color:color:#FF9800;">ОПЛАТИТЬ</b></center>
-	</div></a>
-	
-	
-	<a href="http://paykasa24.com/payment/go/?transaction=3&item=%D0%91%D0%B0%D0%B7%D0%BE%D0%B2%D1%8B%D0%B9%20%D0%BA%D0%B0%D0%B1%D0%B8%D0%BD%D0%B5%D1%82%20trust-signals.com%20%D0%BD%D0%B0%203%20%D0%B4%D0%BD%D1%8F.&amount=799">
-<div class="col-sm-4 payya " style=""><i class="fa fa-suitcase" aria-hidden="true"></i> <span style="font-size:16px">Базовый</span> аккаунт на 3 дня <br><center> 
-   <i class="fa fa-shopping-cart" aria-hidden="true" style="color: grey;
-    font-size: 16px;"></i> <span style="font-size: 22px;
-    color: #607D8B;">799 RUB</span> <span style="color:green;">(14$)</span><br> <b style="color:color:#FF9800;">ОПЛАТИТЬ</b></center>
-	</div></a>
-	
-	
-	
+                    <div class="hidden-xs col-md-12" style="margin-top: 10px;">
+                        <center style="">
+                            <iframe frameborder="0" allowtransparency="true" scrolling="no" style="background: rgba(51, 51, 51, 0.14);"
+                                    src="https://money.yandex.ru/embed/shop.xml?account=410015355400036&quickpay=shop&payment-type-choice=on&writer=seller&targets=%D0%9E%D0%BF%D0%BB%D0%B0%D1%82%D0%B0+%D0%B2%D1%80%D0%B5%D0%BC%D0%B5%D0%BD%D0%B8+Option-Signal&default-sum=&button-text=01&comment=on&hint=%D0%A3%D0%BA%D0%B0%D0%B6%D0%B8%D1%82%D0%B5+%D1%81%D0%B2%D0%BE%D0%B9+Email+%D0%B2+%D1%81%D0%B5%D1%80%D0%B2%D0%B8%D1%81%D0%B5&mail=on&successURL=http%3A%2F%2Ftrust-signals.com%2Fpay%2Fpayok.php" width="450" height="268"></iframe>
 
-</div>
-</center>
-</div>
-
-<div class="col-md-12" style="margin-top: 10px; background-color: rgba(244, 67, 54, 0.74);
+                        </center>
+                    </div>
+                    <div style="margin-top: 10px; background-color: rgba(0, 13, 255, 0.74);
+	padding: 10px;
+	color: white;" class="col-md-12 hidden-sm hidden-md hidden-lg">
+                        <p><b>Реквизиты: </b>410015355400036</p>
+                        <p>1. Оплатить на сумму выбранного Вами пакета</p>
+                        <p>2. Отправить чек с результатом оплаты в техподдержку</p>
+                    </div>
+                    <div class="col-md-12" style="margin-top: 10px; background-color: rgba(244, 67, 54, 0.74);
 	padding: 10px;
 	color: white;"><p class="text-center">
-После проверки платежа администратором, время будет начислено!<br>
-After checking the time of payment by the administrator will be credited!</p>
-</div>
+                            После проверки платежа администратором, время будет начислено!<br>
+                            After checking the time of payment by the administrator will be credited!</p>
+                    </div>
 
 
-			</div>
+                </div>
 
-			</div>
-		</div>
-	</div>
+            </div>
+        </div>
+    </div>
 </div>
 <!--  Neteller  -->
 <div style="overflow-y: auto;" class="modal fade" id="neteller-system" tabindex="-1" role="dialog" aria-labelledby="neteller-system-label" style="z-index: 500000;">
-	<div class="modal-dialog" role="document">
-		<div class="modal-content">
-			<div class="modal-header">
-				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
-				<h4 class="modal-title" id="myModalLabel">Оплата через Neteller</h4>
-			</div>
-			<div class="modal-body" id="chart-box-modal" style="    height: 100%;">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel">Оплата через Neteller</h4>
+            </div>
+            <div class="modal-body" id="chart-box-modal" style="    height: 100%;">
 
 
 
 
-					<div class="row">
-  <div class="col-md-6" style="    background-color: #E6463A;
+                <div class="row">
+                    <div class="col-md-6" style="    background-color: #E6463A;
 	color: white;
 	padding: 10px;"><b>Тарифы VIP раздела.</b><br>
-На 1 неделю -- 25$ <br>
-На 1 месяц + ROBOT-- 45$ <br>
-На 1 Год + ROBOT -- 99$ </div>
+                        На 1 неделю -- 25$ <br>
+                        На 1 месяц + ROBOT-- 45$ <br>
+                        На 1 Год + ROBOT -- 99$ </div>
 
 
 
 
-  <div class="col-md-6" style="background-color: #009688;
+                    <div class="col-md-6" style="background-color: #009688;
 	color: white;
 	padding: 10px;"><b>Тарифы базавого раздела.</b><br>
-3 дня -- 7$ <br>
-7 дней -- 8$ <br>
-30 дней -- 14$ </div>
+                        3 дня -- 299 рублей <br>
+                        7 дней -- 499 рублей <br>
+                        30 дней-- 899 рублей </div>
 
-		<div class="col-md-12" style="margin-top: 10px; font-size: 15px;
+                    <div class="col-md-12" style="margin-top: 10px; font-size: 15px;
 	background: rgba(255, 200, 0, 0.28);">
-Для оплаты тарифа, переводите нужную сумму на Email: pay@trust-signals.com <br>
-To pay for the fare , transfer the required amount to Email: pay@trust-signals.com
-</div>
+                        Для оплаты тарифа, переводите нужную сумму на Email: pay@trust-signals.com <br>
+                        To pay for the fare , transfer the required amount to Email: pay@trust-signals.com
+                    </div>
 
-<div class="col-md-12" style="margin-top: 10px;">
-
-
-<img src="/engine/templates/default/img/nett.png" style="width: 100%;text-align: center; max-width:570px">
-
-</div>
+                    <div class="col-md-12" style="margin-top: 10px;">
 
 
-		<div class="col-md-12" style="margin-top: 10px; background-color: rgba(244, 67, 54, 0.74);
+                        <img src="/engine/templates/default/img/nett.png" style="width: 100%;text-align: center; max-width:570px">
+
+                    </div>
+
+
+                    <div class="col-md-12" style="margin-top: 10px; background-color: rgba(244, 67, 54, 0.74);
 	padding: 10px;
 	color: white;"><center>
-После проверки платежа администратором, время будет начислено!<br>
-After checking the time of payment by the administrator will be credited!</center>
-</div>
+                            После проверки платежа администратором, время будет начислено!<br>
+                            After checking the time of payment by the administrator will be credited!</center>
+                    </div>
 
 
 
 
-			</div>
-		</div>
-	</div>
-</div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 <!--  Skrill  -->
 <div style="overflow-y: auto;" class="modal fade" id="skrill-system" tabindex="-1" role="dialog" aria-labelledby="skrill-system-label" style="z-index: 500000;">
-	<div class="modal-dialog" role="document">
-		<div class="modal-content">
-			<div class="modal-header">
-				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
-				<h4 class="modal-title" id="myModalLabel">Оплата через Skrill</h4>
-			</div>
-			<div class="modal-body" id="chart-box-modal">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel">Оплата через Skrill</h4>
+            </div>
+            <div class="modal-body" id="chart-box-modal">
 
 
-				 <div class="row">
-  <div class="col-md-6" style="    background-color: #E6463A;
+                <div class="row">
+                    <div class="col-md-6" style="    background-color: #E6463A;
 	color: white;
 	padding: 10px;"><b>Тарифы VIP раздела.</b><br>
-      На 1 неделю -- 25$ <br>
-      На 1 месяц + ROBOT-- 45$ <br>
-      На 1 Год + ROBOT -- 99$ </div>
+                        На 1 неделю -- 25$ <br>
+                        На 1 месяц + Robot -- 45$ <br>
+                        На 1 Год + Robot -- 99$ </div>
 
 
 
 
-  <div class="col-md-6" style="background-color: #009688;
+                    <div class="col-md-6" style="background-color: #009688;
 	color: white;
 	padding: 10px;"><b>Тарифы базавого раздела.</b><br>
-      3 дня -- 7$ <br>
-      7 дней -- 8$ <br>
-      30 дней -- 14$ </div>
+                        3 дня -- 5$ <br>
+                        7 дней -- 7$ <br>
+                        30 дней -- 15$ </div>
 
-		<div class="col-md-12" style="margin-top: 10px; font-size: 15px;
+                    <div class="col-md-12" style="margin-top: 10px; font-size: 15px;
 	background: rgba(255, 200, 0, 0.28);">
-Для оплаты тарифа, переводите нужную сумму на Email: pay@trust-signals.com <br>
-To pay for the fare , transfer the required amount to Email: pay@trust-signals.com
-</div>
+                        Для оплаты тарифа, переводите нужную сумму на Email: pay@trust-signals.com <br>
+                        To pay for the fare , transfer the required amount to Email: pay@trust-signals.com
+                    </div>
 
 
 
-		<div class="col-md-12" style="margin-top: 10px; background-color: rgba(244, 67, 54, 0.74);
+                    <div class="col-md-12" style="margin-top: 10px; background-color: rgba(244, 67, 54, 0.74);
 	padding: 10px;
 	color: white;"><center>
-После проверки платежа администратором, время будет начислено!<br>
-After checking the time of payment by the administrator will be credited!</center>
-</div>
+                            После проверки платежа администратором, время будет начислено!<br>
+                            After checking the time of payment by the administrator will be credited!</center>
+                    </div>
 
 
 
 
 
 
-			</div>
-		</div>
-	</div>
-</div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
